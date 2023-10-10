fc65da6458dfa4c108f438ded3a05b9837b01cb9
Keivan-sf <keyvan0082@gmail.com>
2023-10-10 15:01:12 +0330

    chore: create `uri_identifier` module in parser

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,5 @@
-use std::process::exit;
-
 mod parser;
+
 fn main() {
-    let protocol = parser::get_uri_protocol("vmess://");
-    match protocol {
-        Some(parser::Protocols::Vless) => {
-            println!("The protocol was Vless");
-        }
-        Some(_) => {
-            println!("Some recognizable protocol")
-        }
-        None => {
-            println!("The protocol is not supported");
-            exit(0);
-        }
-    }
+    parser::parse("vmess://test");
 }
