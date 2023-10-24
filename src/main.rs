19a825c0490819b467d1f93b71b8ca43b52054f4
Keivan-sf <keyvan0082@gmail.com>
2023-10-24 17:00:00 +0330

    Add clap for parsing cli args

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,27 @@
 mod parser;
+use clap::{Parser, Subcommand};
+use std::path::PathBuf;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    uri: String,
+    #[arg(short, long, value_name = "socksport")]
+    socksport: Option<u16>,
+}
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    let uri = args.get(1).unwrap();
-    parser::parse(uri);
+    let cli = Cli::parse();
+    match cli.socksport {
+        Some(port) => {
+            println!("the port: {}", port)
+        }
+        None => {
+            println!("the port is not here")
+        }
+    };
+    println!("the uri is: {}", cli.uri);
+    // let args: Vec<String> = std::env::args().collect();
+    // let uri = args.get(1).unwrap();
+    // parser::parse(uri);
 }
